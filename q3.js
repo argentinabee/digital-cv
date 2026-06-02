9b158c5266bf819131854013d00f7dcb785cd66b
argentinabee <crystal.gardner82@gmail.com>
2026-06-02 14:45:42 -0400

    Create q3.js

---
+++ q3.js
@@ -0,0 +1,9 @@
+const hostName = "Julius Caesar";
+const friendName = "Brutus";
+const shiftValue = 3;
+
+let partyLocation = "GARDEN";
+let encryptedLocation = "JDUGHQ";
+let invitationMessage = "JDUGHQ";
+let isPartySecret = true;
+let numberOfGuests = 25;
