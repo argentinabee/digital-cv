f424817865c8c023c7b2180dce7ef65eb6921c48
argentinabee <crystal.gardner82@gmail.com>
2026-06-02 15:23:46 -0400

    Delete q2.js

--- q2.js
+++
@@ -1,8 +0,0 @@
-let partyLocation = "GARDEN";          // String
-let encryptedLocation = "JDUGHQ";      // String
-let shiftValue = 3;                    // Number
-let hostName = "Julius Caesar";        // String
-let friendName = "Brutus";             // String
-let invitationMessage = "JDUGHQ";      // String
-let isPartySecret = true;              // Boolean
-let numberOfGuests = 25;               // Number
