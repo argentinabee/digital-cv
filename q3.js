b06ae4f9a3c6448cc3ab9ac8fa40dd903b305261
argentinabee <crystal.gardner82@gmail.com>
2026-06-02 15:23:55 -0400

    Delete q3.js

--- q3.js
+++
@@ -1,9 +0,0 @@
-const hostName = "Julius Caesar";
-const friendName = "Brutus";
-const shiftValue = 3;
-
-let partyLocation = "GARDEN";
-let encryptedLocation = "JDUGHQ";
-let invitationMessage = "JDUGHQ";
-let isPartySecret = true;
-let numberOfGuests = 25;
