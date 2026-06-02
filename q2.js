7f5c7cac34896fb93dbd7d946e2e17847cd9237c
argentinabee <crystal.gardner82@gmail.com>
2026-06-02 14:45:04 -0400

    Create q2.js

---
+++ q2.js
@@ -0,0 +1,8 @@
+let partyLocation = "GARDEN";          // String
+let encryptedLocation = "JDUGHQ";      // String
+let shiftValue = 3;                    // Number
+let hostName = "Julius Caesar";        // String
+let friendName = "Brutus";             // String
+let invitationMessage = "JDUGHQ";      // String
+let isPartySecret = true;              // Boolean
+let numberOfGuests = 25;               // Number
